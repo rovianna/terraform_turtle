c2f9643955144f246875c03448c159ede1ed0c7d
Rodrigo Vianna <Rodrigo@Estefannies-MacBook-Pro.local>
2023-05-31 18:03:47 -0300

    The file is beeing uploaded as a local file. To do: Python script to fetch the data from GSheets and upload to s3 as a script

--- main.tf
+++ main.tf
@@ -6,3 +6,11 @@ resource "aws_s3_bucket" "data_bucket" {
     bucket = "mask-amino"
     acl = "private"
 }
+
+resource "null_resource" "upload_file" {
+    provisioner "local-exec" {
+      command = "aws s3 cp ./consolidade_med.csv s3://${aws_s3_bucket.data_bucket.bucket}/consolidado_med.csv"
+    }
+
+    depends_on = [ aws_s3_bucket.data_bucket ]
+}
